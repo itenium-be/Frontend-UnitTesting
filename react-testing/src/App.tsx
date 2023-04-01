aec8e979f373b147fa59342fc0313e26d07f2e5a
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-04-01 21:35:07 +0200

    added bootstrap, header, font-awesome

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -1,9 +1,32 @@
+
+
+
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="/">React Testing</a>
-      </header>
+      <Header />
     </div>
+  );
+}
+
+
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src="/favicon.png" style={{marginRight: 18, width: 24}} />
+          React Testing
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="/">Storybook</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
